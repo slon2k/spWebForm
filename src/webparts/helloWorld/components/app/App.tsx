136008ebc074608f575aa2d5a9a5923662854157
Boris Korobeinikov <slon2k@gmail.com>
2020-02-21 17:26:55 +0600

    getting items from List

--- src/webparts/helloWorld/components/app/App.tsx
+++ src/webparts/helloWorld/components/app/App.tsx
@@ -1,16 +1,39 @@
 import * as React from "react";
 import { IAppProps } from "./IAppProps";
 import styles from "./App.module.scss";
+import { SPHttpClient } from "@microsoft/sp-http";
 
-const App: React.FC<IAppProps> = props => {
+const App: React.FC<IAppProps> = ({ spHttpClient, currentSiteUrl }) => {
+  const [items, setItems] = React.useState([]);
+  const [loadingItems, setLoadingItems] = React.useState(false);
+  const endpoint: string = `${currentSiteUrl}/_api/web/lists/GetByTitle('Helpdesk')/items`;
+
   React.useEffect(() => {
-    console.log("Effect", props);
+    setLoadingItems(true);
+    spHttpClient
+      .get(endpoint, SPHttpClient.configurations.v1)
+      .then(res => res.json())
+      .then(res => setItems(res.value))
+      .then(() => setLoadingItems(false))
+      .catch(e => console.error(e));
   }, []);
+
+  if (loadingItems) {
+    return <div>Loading ...</div>;
+  }
+
+  console.log("Items: ", items);
+
   return (
     <div className={styles.App}>
       <div className={styles.container}>
         <h1 className={styles.title}>App</h1>
-        <p>{props.currentSiteUrl}</p>
+        <p>{currentSiteUrl}</p>
+        <ul>
+          {items.map(item => (
+            <li key={item.Id}>{item.Title}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
