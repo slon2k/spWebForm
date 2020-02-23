18d9c67dcaeac2cf983189f8a4ba10f6ac21e87c
Boris Korobeinikov <slon2k@gmail.com>
2020-02-23 22:04:37 +0600

    requests

--- src/webparts/helloWorld/components/app/App.tsx
+++ src/webparts/helloWorld/components/app/App.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { IAppProps } from "./IAppProps";
 import styles from "./App.module.scss";
+import HomePage from "./../../pages/HomePage"
+import FormPage from "./../../pages/FormPage";
 import { SPHttpClient } from "@microsoft/sp-http";
 import { sp } from "@pnp/sp";
 import "@pnp/sp/webs";
 import "@pnp/sp/lists";
 import "@pnp/sp/items";
+import '@pnp/sp/site-users';
+
 
 const App: React.FC<IAppProps> = ({ spHttpClient, currentSiteUrl }) => {
   const [items, setItems] = React.useState([]);
@@ -24,9 +29,30 @@ const App: React.FC<IAppProps> = ({ spHttpClient, currentSiteUrl }) => {
 
   const fetchItems = async () => {
     const list = sp.web.lists.getByTitle("Helpdesk");
-    const r = await list.items.getAll();
+    const r = await list.items.select("Id", "Title", "Author/Id", "Author/Title").expand("Author/Id").getAll();
     console.log(r);
   };
+
+  const fetchItem = async () => {
+    const list = sp.web.lists.getByTitle("Helpdesk");
+    const r = await list.items.getById(2).select('Comments', 'Status', 'Editor/Id', 'Versions').expand('Versions').get();
+    console.log(r);
+  };
+
+  const fetchUser = async () => {
+    const user = await sp.web.currentUser.get();
+    console.log(user);
+  }
+
+  const fetchList = async () => {
+    const list = await sp.web.lists.getByTitle("Helpdesk").get();
+    console.log(list);
+  }
+
+  const addItem = async () => {
+    const result = await sp.web.lists.getByTitle("Helpdesk").items.add({ Title: "Next item" });
+    result.item.get().then(r => console.log(r));
+  }
 
   if (loadingItems) {
     return <div>Loading ...</div>;
@@ -35,18 +61,33 @@ const App: React.FC<IAppProps> = ({ spHttpClient, currentSiteUrl }) => {
   console.log("Items: ", items);
 
   return (
-    <div className={styles.App}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>App</h1>
-        <p>{currentSiteUrl}</p>
-        <ul>
-          {items.map(item => (
-            <li key={item.Id}>{item.Title}</li>
-          ))}
-        </ul>
+    <Router>
+      <div className={styles.App}>
+        <div className={styles.container}>
+          <h1 className={styles.title}>App</h1>
+          <div>
+            <h2>Links:</h2>
+            <Link to="/">Home</Link>
+            <Link to="/form">Form</Link>
+          </div>
+          <Switch>
+            <Route path="/form"><FormPage /></Route>
+            <Route path="/" exact ><HomePage/></Route>
+          </Switch>
+          <p>{currentSiteUrl}</p>
+          <ul>
+            {items.map(item => (
+              <li key={item.Id}>{item.Title}</li>
+            ))}
+          </ul>
+        </div>
+        <button onClick={fetchItems}>fetch items</button>
+        <button onClick={fetchItem}>fetch item</button>
+        <button onClick={fetchUser}>fetch user</button>
+        <button onClick={fetchList}>fetch list</button>
+        <button onClick={addItem}>add item</button>
       </div>
-      <button onClick={fetchItems}>fetch</button>
-    </div>
+    </Router>
   );
 };
 
---
+++ src/webparts/helloWorld/pages/FormPage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const FormPage = () => {
+  return (
+    <div>
+      <h1>Form page</h1>
+    </div>
+  )
+}
+
+export default FormPage
