6f3400332e77b06b36e923a9a34e4ff640469fe2
Boris Korobeinikov <slon2k@gmail.com>
2020-02-25 17:21:40 +0600

    useEffect fix

--- src/webparts/helloWorld/pages/DetailsPage.tsx
+++ src/webparts/helloWorld/pages/DetailsPage.tsx
@@ -24,7 +24,7 @@ const DetailsPage: React.FC<RouteComponentProps<IParams>> = ({ match }) => {
 
   React.useEffect(() => {
     fetchItem();
-  }, []);
+  }, [id]);
 
   if (loading) {
     return <div>Loading ticket ...</div>;
